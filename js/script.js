b238211da44de6625c7cd8f7d47203157ff58dca
Radoslaw Kowalczyk <rkowal64@gmail.com>
2022-10-06 13:20:44 +0200

    changed function

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
-function welcome () {
-    console.log("Hej, to moja pierwsza strona, którą stworzyłem... na dodatek o sobie :)");
+{
+    const welcome = () => {
+        console.log("Hej, to moja pierwsza strona, którą stworzyłem... na dodatek o sobie :)");
+    }
+    
+    const onChangeBackgroundClick = () => {
+        bodyElement.classList.toggle("body--lighter");
+        if (nextColorNameElement.innerText === "jasne") {
+            nextColorNameElement.innerText = "ciemne";
+        } else {
+            nextColorNameElement.innerText = "jasne";
+        }
+    };
+    
+    const removePhotoClick = () => {
+        photoElement.classList.toggle("photoRemove");
+        if (buttonElement.innerText === "Ukryj zdjęcie") {
+            buttonElement.innerText = "Pokaż zdjęcie";
+        } else {
+            buttonElement.innerText = "Ukryj zdjęcie";
+        }
+    };
+    
+    welcome();
+    
+    const buttonElement = document.querySelector(".js-button");
+    const photoElement = document.querySelector(".js-image");
+    const changeButtonElement = document.querySelector(".js-changeBackgroundButton");
+    const bodyElement = document.querySelector(".js-body");
+    const nextColorNameElement = document.querySelector(".js-nextColorName");
+    
+    buttonElement.addEventListener("click", removePhotoClick);
+    
+    changeButtonElement.addEventListener("click", onChangeBackgroundClick);
 }
-
-welcome();
-
-let buttonElement = document.querySelector(".js-button");
-let photoElement = document.querySelector(".js-image");
-let changeButtonElement = document.querySelector(".js-changeBackgroundButton");
-let bodyElement = document.querySelector(".js-body");
-let nextColorNameElement = document.querySelector(".js-nextColorName");
-
-buttonElement.addEventListener("click", () => {
-    photoElement.classList.toggle("photoRemove");
-    if (buttonElement.innerText === "Ukryj zdjęcie") {
-        buttonElement.innerText = "Pokaż zdjęcie";
-    } else {
-        buttonElement.innerText = "Ukryj zdjęcie";
-    }
-});
-
-changeButtonElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("body--lighter");
-    if (nextColorNameElement.innerText === "jasne") {
-        nextColorNameElement.innerText = "ciemne";
-    } else {
-        nextColorNameElement.innerText = "jasne";
-    }
-});
