f17d45bc58cfb0f68f46fe470235b3206fe68036
Radoslaw Kowalczyk <rkowal64@gmail.com>
2022-10-07 14:35:51 +0200

    changed if to ternary in toggleBackground

--- js/script.js
+++ js/script.js
@@ -3,21 +3,19 @@
         console.log("Hej, to moja pierwsza strona, którą stworzyłem... na dodatek o sobie :)");
     }
 
-    const backgroundToggle = () => {
+    const toggleBackground = () => {
         const bodyElement = document.querySelector(".js-body");
         const nextColorNameElement = document.querySelector(".js-nextColorName");
         bodyElement.classList.toggle("body--lighter");
-        if (nextColorNameElement.innerText === "jasne") {
-            nextColorNameElement.innerText = "ciemne";
-        } else {
-            nextColorNameElement.innerText = "jasne";
-        }
+
+        nextColorNameElement.innerText = bodyElement.classList.contains("body--lighter") ? "ciemne" : "jasne";
     };
 
     const removePhotoClick = () => {
         const photoElement = document.querySelector(".js-image");
         const changeTextElement = document.querySelector(".js-changeText");
         photoElement.classList.toggle("photoRemove");
+        
         if (changeTextElement.innerText === "Ukryj") {
             changeTextElement.innerText = "Pokaż";
         } else {
@@ -30,10 +28,10 @@
 
         const buttonElement = document.querySelector(".js-button");
         const changeButtonElement = document.querySelector(".js-changeBackgroundButton");
-        
+
         buttonElement.addEventListener("click", removePhotoClick);
 
-        changeButtonElement.addEventListener("click", backgroundToggle);
+        changeButtonElement.addEventListener("click", toggleBackground);
     }
 
     init();
